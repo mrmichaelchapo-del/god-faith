8fa978cdefe196609c528d07299bd9758f847aa3
mrmichaelchapo-del <mr.michaelchapo@gmail.com>
2026-09-07 15:11:04 -0400

    Create prayers.js

---
+++ prayers.js
@@ -0,0 +1,11 @@
+<!--- Prayer 1 --->
+function prayer() {
+  console.log("Dear God,");
+  console.log("Thank You for this day and for the people and blessings in my life.");
+  console.log("Please give me wisdom, strength, and kindness.");
+  console.log("Help me make good choices and treat others with compassion.");
+  console.log("Please watch over my family, friends, and everyone who needs help.");
+  console.log("Amen.");
+}
+
+prayer();
