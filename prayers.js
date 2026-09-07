cde6e389c95d410663b925b7db062675d97aca14
mrmichaelchapo-del <mr.michaelchapo@gmail.com>
2026-09-07 15:13:46 -0400

    Update prayers.js

--- prayers.js
+++ prayers.js
@@ -9,3 +9,17 @@ function prayer() {
 }
 
 prayer();
+
+<!--- Prayer 2 --->
+function prayer() {
+  console.log("Dear God,");
+  console.log("Thank You for giving me another day.");
+  console.log("Help me stay hopeful when things are difficult.");
+  console.log("Give me patience when I am frustrated and courage when I am afraid.");
+  console.log("Help me learn from my mistakes and grow into a better person.");
+  console.log("Please bring peace, safety, and kindness to those around me.");
+  console.log("Guide me in everything I do.");
+  console.log("Amen.");
+}
+
+prayer();
